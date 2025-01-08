e37180e974f06ea3e49a33cba7b4a3e6a20a2baf
dinesh7733@outlook.com <dinesh7733@outlook.com>
2025-01-08 12:28:55 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-document.addEventListener("scroll", function () {
-  const navbar = document.getElementById("navbar-top");
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
+window.addEventListener('resize', () => {
+  document.body.style.overflowX = 'hidden';
 });
